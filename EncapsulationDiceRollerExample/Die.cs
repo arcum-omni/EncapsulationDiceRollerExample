f900c63383257ceb79b1fcbc3c5188fe74cc6601
arcum-omni <hut8dev@gmail.com>
2020-01-13 14:52:32 -0800

    Refactored main, cleaned up code

--- EncapsulationDiceRollerExample/Die.cs
+++ EncapsulationDiceRollerExample/Die.cs
@@ -11,22 +11,43 @@ namespace EncapsulationDiceRollerExample
     /// </summary>
     class Die
     {
-        // static members get shared across all instances of this class
+        /// <summary>
+        /// static members get shared across all instances of this class
+        /// </summary>
         private static Random rand;
+        private byte faceValue;
+
+        /// <summary>
+        /// Runs once before any Die objects are created and initializes Random object once
+        /// </summary>
+        static Die()
+        {
+            rand = new Random();
+        }
 
         /// <summary>
         /// Set initial value of die
         /// </summary>
         public Die()
         {
-            rand = new Random();
             Roll();
         }
 
         /// <summary>
         /// The current value of a the die (1-6)
         /// </summary>
-        public byte FaceValue { get; private set; }
+        public byte FaceValue
+        {
+            get { return faceValue; }
+            private set
+            {
+                if (value < 1 || value > 6)
+                {
+                    throw new ArgumentException("Invalid Face Value");
+                }
+                faceValue = value; 
+            }
+        }
 
         /// <summary>
         /// Die that are held, are not rolled.
@@ -40,10 +61,16 @@ namespace EncapsulationDiceRollerExample
         /// </summary>        
         public byte Roll()
         {
+            // If current die is held, return the current value
+            if (IsHeld)
+            {
+                return FaceValue;
+            }
+
             const byte MinValue = 1; // min die roll
             const byte MaxValue = 6; // max die roll
             const byte Offset = 1;   // offset for Random.Next(lower, upper) lower bound is inclusive, upper bound is exclusive.
-            
+
             byte newValue = (byte)rand.Next(MinValue, MaxValue + Offset); // cast from int to byte
 
             FaceValue = newValue;
--- EncapsulationDiceRollerExample/Program.cs
+++ EncapsulationDiceRollerExample/Program.cs
@@ -8,34 +8,75 @@ namespace EncapsulationDiceRollerExample
 {
     class Program
     {
+        /// <summary>
+        /// Main needs 10 lines or less
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Die[] dice = new Die[5];
+            Die[] dice = new Die[5]; // Create dice array
+            InitDice(dice);
+            Console.WriteLine("Initial Dice Face Values"); 
+            PrintDieFaceValues(dice);
+            HoldDice(dice);
+            Console.WriteLine("\nDice 1-3 Are Held,\nDice Re-Rolled");
+            RollAllDice(dice);
+            PrintDieFaceValues(dice);
+            Console.ReadKey();
+        }
 
+        /// <summary>
+        /// Roll Dice in Array
+        /// </summary>
+        /// <param name="dice"></param>
+        private static void RollAllDice(Die[] dice)
+        {
+            // ReRoll Dice
             for (int i = 0; i < dice.Length; i++)
             {
-                dice[i] = new Die();
+                dice[i].Roll();
             }
+        }
 
+        /// <summary>
+        /// Holds first three dice,
+        /// For testing purposes.
+        /// </summary>
+        /// <param name="dice"></param>
+        private static void HoldDice(Die[] dice)
+        {
+            // Hold dice 1-3
+            for (int i = 0; i < 3; i++)
+            {
+                dice[i].IsHeld = true;
+            }
+        }
+
+        /// <summary>
+        /// Displays die face values
+        /// </summary>
+        /// <param name="dice"></param>
+        private static void PrintDieFaceValues(Die[] dice)
+        {
+            // Display the dice face values
             for (int i = 0; i < dice.Length; i++)
             {
                 Console.WriteLine(dice[i].FaceValue);
             }
+        }
 
-
-            /*
-            Die myDice = new Die();
-            
-
-            Console.WriteLine(myDice.FaceValue);
-
-            myDice.Roll();
-
-            Console.WriteLine(myDice.FaceValue);
-
-            */
-
-            Console.ReadKey();
+        /// <summary>
+        /// Initializes all die in a given array,
+        /// Assumes array has a length
+        /// </summary>
+        /// <param name="dice"></param>
+        private static void InitDice(Die[] dice)
+        {
+            // Instantiate 5 dice
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+            }
         }
     }
 }
